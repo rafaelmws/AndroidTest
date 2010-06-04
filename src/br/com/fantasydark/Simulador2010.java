4109754043c97beee91f83f2338d0d1351510f11
Rafael Martins <rafael.martins@corp.globo.com>
2010-06-04 10:06:27 -0300

    first commit

---
+++ src/br/com/fantasydark/Simulador2010.java
@@ -0,0 +1,15 @@
+package br.com.fantasydark;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Simulador2010 extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
